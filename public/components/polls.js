63a1aaaeb98a3f4ba983812757a45d7199e66182
Elisha Terada <elishaterada@gmail.com>
2016-10-16 00:23:04 -0700

    Share short URL

--- public/components/polls.js
+++ public/components/polls.js
@@ -9,8 +9,9 @@ angular
     }
   })
 
-function PollsCtrl (Profiles, Polls, $stateParams, $firebaseObject, $firebaseArray, $mdToast, $localStorage, moment) {
+function PollsCtrl (Profiles, Polls, $stateParams, $firebaseObject, $firebaseArray, $mdToast, $localStorage, $http, moment, $window) {
   var ctrl = this
+  var clipboard = new $window.Clipboard('.clipboard')
 
   ctrl.$onInit = function () {
     ctrl.profiles = $firebaseObject(Profiles)
@@ -21,7 +22,25 @@ function PollsCtrl (Profiles, Polls, $stateParams, $firebaseObject, $firebaseArr
     })
 
     ctrl.voted = $localStorage[$stateParams.id]
+
+    ctrl.shortURL = getShortURL()
   }
+
+  clipboard.on('success', function (e) {
+    $mdToast.show(
+      $mdToast.simple()
+        .textContent('Copied URL')
+        .hideDelay(3000)
+    )
+  })
+
+  clipboard.on('error', function (e) {
+    $mdToast.show(
+      $mdToast.simple()
+        .textContent('Copy Failed')
+        .hideDelay(3000)
+    )
+  })
 
   ctrl.vote = function (choice) {
     // Update Poll
@@ -30,7 +49,6 @@ function PollsCtrl (Profiles, Polls, $stateParams, $firebaseObject, $firebaseArr
 
     ctrl.polls.$save(ctrl.poll)
       .then(function () {
-
         $localStorage[$stateParams.id] = true
         ctrl.voted = $localStorage[$stateParams.id]
 
@@ -39,8 +57,18 @@ function PollsCtrl (Profiles, Polls, $stateParams, $firebaseObject, $firebaseArr
             .textContent('Vote Casted')
             .hideDelay(3000)
         )
-
       })
   }
 
+  function getShortURL () {
+    $http({
+      method: 'POST',
+      url: 'https://www.googleapis.com/urlshortener/v1/url',
+      params: { key: 'AIzaSyApu8K6ALu_piGLcSAoP3fuZcPOo0PuT7c' },
+      data: {'longUrl': $window.location.href}
+    })
+      .then(function (res) {
+        ctrl.shortURL = res.data.id
+      })
+  }
 }
