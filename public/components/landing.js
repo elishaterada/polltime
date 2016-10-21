5271bd708f8d1892b00ea1c0f0d01f4d25fb9e28
Elisha Terada <elishaterada@gmail.com>
2016-10-20 23:46:16 -0700

    Support more choices

--- public/components/landing.js
+++ public/components/landing.js
@@ -12,32 +12,43 @@ angular
 function LandingCtrl (Profiles, Polls, $firebaseObject, $firebaseArray, $mdToast, $state, moment) {
   var ctrl = this
 
+  var pollDefault = {
+    question: '',
+    answers: ['', '']
+  }
+
   ctrl.$onInit = function () {
     ctrl.profiles = $firebaseObject(Profiles)
     ctrl.polls = $firebaseArray(Polls)
-    ctrl.newPoll = {
-      question: '',
-      answers: [ {
-        text: '',
-        count: 0
-      }, {
-        text: '',
-        count: 0
-      } ]
-    }
+    ctrl.newPoll = angular.copy(pollDefault)
+  }
+
+  ctrl.cancel = function () {
+    ctrl.newPoll = angular.copy(pollDefault)
   }
 
   ctrl.createPoll = function (newPoll) {
     var uid = null
+    var answers = []
 
     if (ctrl.user) {
       uid = ctrl.user.uid
     }
 
+    // Build answers format
+    _.each(newPoll.answers, function (value, key) {
+      if (value) {
+        answers.push({
+          text: value,
+          count: 0
+        })
+      }
+    })
+
     var pollData = {
       'ownerID': uid,
       'question': newPoll.question,
-      'answers': newPoll.answers,
+      'answers': answers,
       'created': moment().format(),
       'modified': moment().format()
     }
@@ -55,5 +66,13 @@ function LandingCtrl (Profiles, Polls, $firebaseObject, $firebaseArray, $mdToast
             .hideDelay(3000)
         )
       })
+  }
+
+  ctrl.$doCheck = function () {
+    // Automatically append next answer
+    // IF: last item has a value
+    if (_.last(ctrl.newPoll.answers) !== '') {
+      ctrl.newPoll.answers.push('')
+    }
   }
 }
